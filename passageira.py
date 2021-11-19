b160b9b4f3a90777c494d61d0ec7c17c828a374c
manuhon99 <manuhon99@gmail.com>
2021-11-19 17:04:48 -0300

    gitignore, requirements file and json users files

--- passageira.py
+++ passageira.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import sys
-import Pyro4
 import Pyro4.util
+import json
 
 sys.excepthook = Pyro4.util.excepthook
 
@@ -12,12 +12,14 @@ servidor = Pyro4.Proxy("PYRONAME:servidor.carona")
 # um potencial passageiro tem interesse numa viagem como um carona e se cadastra, anunciando interesse em pegar viagens
 
 # passageiro
+def carrega_passageiras():
+    with open('listaPassageiras.json.json') as json_file:
+        lista_passagerias = json.load(json_file)
+    print(lista_passagerias)
+    return lista_passagerias
 
 # cadastra a carona, verifica a lista de interesse para viagens cadastradas por motoristas, inclui a corrida na lista de interesse
 def consulta(idUser):
-    if(not(idUser)):
-        print("Antes de agendar a viagem, precisamos de um cadastro!\n")
-        idUser = cadastro()
     destino = input("Para onde deseja ir? ").strip()
     origem = input("Aonde está? ").strip()
     data = input("Quando deseja ir? ").strip()
@@ -25,59 +27,46 @@ def consulta(idUser):
     if origem and destino and data:
         respConsulta = servidor.consulta(origem, destino, data, 0)
         idCorrida = interesse(data, origem, idUser, destino, qtdePessoas)
-        if(not(respConsulta)):
+        if (not (respConsulta)):
             adicionarALista = input(
                 "Não encontrei nada deseja adicionar a sua lista de interesse? 1 - SIM/ 0 - NÃO\n").strip()
             if adicionarALista == '0':
                 print('Tudo bem! Nos vemos na próxima\n')
-                servidor.cancelarInteresseEmCarona(idCorrida)
+                servidor.cancelar_interesse_em_carona(idCorrida)
         else:
-            servidor.cancelarInteresseEmCarona(idCorrida)
+            servidor.cancelar_interesse_em_carona(idCorrida)
             print(respConsulta)
+
 
 # Registro de interesse em eventos (1,1)
 def interesse(data, origem, idUser, destino, qtdePessoas):
-
-    id = servidor.interesseEmCarona(idUser, origem, destino, data, qtdePessoas, signature)
+    id = servidor.interesse_em_carona(idUser, origem, destino, data, qtdePessoas)
     return id
 
-#cadastro de usuario
-def cadastro ():
-    print("Novo por aqui? Cadastre-se\n")
-    nome = input("Qual seu nome? ").strip().encode()
-    telefone = input("Certo! \n Qual seu telefone?").strip().encode()
-    encryptor = PKCS1_OAEP.new(key)
-    nome = encryptor.encrypt(nome)
-    telefone = encryptor.encrypt(telefone)
-    if nome and telefone:
-        idUser = servidor.cadastroUsuario(nome, telefone, publickey, 0) #O ultimo campo - se 1 motorista, se 0 passageiro
-        print(idUser)
-    return idUser
 
-#remocao da lista de interesse
-def removeInteresse(idUser):
+# remocao da lista de interesse
+def remove_interesse(idUser):
     remover = input("Digite o número da viagem que deseja remover").strip()
-    servidor.cancelarInteresseEmCarona(remover)
+    servidor.cancelar_interesse_em_carona(remover)
     print("Feito! Nos vemos na próxima!\n")
-#TODO
+
+
+# TODO
 
 def main():
-
+    carrega_passageiras()
     escolha = ''
     idUser = ''
     while escolha != '0':
         escolha = input("\nOlá, o que precisa hoje? \n "
-                        "1 - Cadastro \n"
                         " 2 - Buscar viagem \n"
                         " 3 - Remover Viagem \n"
                         " 0 - Sair\n").strip()
         if escolha != '0':
-            if escolha == '1':
-                idUser = cadastro()
-            elif escolha == '2':
+            if escolha == '2':
                 consulta(idUser)
             else:
-                removeInteresse(idUser)
+                remove_interesse(idUser)
     print("Até a próxima!")
 
 
